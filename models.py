3164a09bba4d12fce36ae2665bb8b3a4fb7a89ba
IvashovaVD <99237542+IvashovaVD@users.noreply.github.com>
2022-03-08 08:57:33 +0300

    feature: adding and adjusting fields

--- models.py
+++ models.py
@@ -5,6 +5,11 @@ from django.db import models
 
 
 @final
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
 class RegUsers(models.Model):
     emailu = models.CharField(max_length=50)
     parol = models.CharField(max_length=30)
@@ -13,16 +18,17 @@ class RegUsers(models.Model):
 
 
 class Folders(models.Model):
-    name = models.CharField(max_length=100)
-    release_date = models.DateTimeField(auto_now_add=True)
     num_user = models.ForeignKey(RegUsers, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default="Доска")
+    release_date = models.DateTimeField(auto_now_add=True)
 
 
 class Notes(models.Model):
     num_folder = models.ForeignKey(Folders, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    textf = models.TextField()
-    filef = models.FileField()
+    name = models.CharField(max_length=100, default="Заметка")
+    filen = models.FileField(upload_to=user_directory_path)
+    textn = models.TextField()
+    urln = models.URLField()
     available = models.BooleanField()
 
     created_at = models.DateTimeField(auto_now_add=True)
